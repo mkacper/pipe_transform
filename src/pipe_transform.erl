df84d034b8aa837ffff3119d5748c9f36b5f0068
Kacper Mentel <mentel.kk@gmail.com>
2020-02-18 10:32:57 +0100

    Handle funs

--- src/pipe_transform.erl
+++ src/pipe_transform.erl
@@ -2,25 +2,36 @@
 -export([parse_transform/2]).
 
 parse_transform(Ast, _Opt) ->
-    [parse(A) || A <- Ast].
+    %io:format ("AST: ~p~n", [Ast]),
+    NewAst = [parse(A) || A <- Ast],
+    %io:format("NewAST: ~p~n", [NewAst]),
+    NewAst.
 
-% Helpers
+% Parsing
 
 parse({function, Line, FunName, ArgNum, Clause}) ->
-    {function, Line, FunName, ArgNum, [parse(X) || X <- Clause]};
+    {function, Line, FunName, ArgNum, [parse(C) || C <- Clause]};
 
 parse({clause, Line, Vars, Gaurd, ExprCalls}) ->
-    {clause, Line, Vars, Gaurd, parse(ExprCalls)};
+    {clause, Line, Vars, Gaurd, [parse(E) || E <- ExprCalls]};
 
-parse([{call, _, {remote, _, {atom, _, pipe_transform}, {atom, _, pipe}}, [{Type, _, Val} | PipedCalls]}]) ->
-    [lists:foldl(fun(CCall, {}) ->
-                     NewArgs = [{Type, 0, Val} | element(size(CCall), CCall)],
-                     replace_args_for_call(CCall, NewArgs);
-                    (CCall, ArgCall) ->
-                     replace_args_for_call(CCall, [ArgCall | element(size(CCall), CCall)])
-                end, {}, PipedCalls)];
+parse({match, Line, Var, Expr}) ->
+    {match, Line, Var, parse(Expr)};
 
-parse(X) -> X.
+parse({'fun', Line, {clauses, Clauses}}) ->
+    {'fun', Line, {clauses, [parse(C) || C <- Clauses]}};
+
+parse({call, _, {remote, _, {atom, _, pipe_transform}, {atom, _, pipe}}, [{Type, _, Val} | PipedCalls]}) ->
+    lists:foldl(fun(Call, {}) ->
+                    NewArgs = [{Type, 0, Val} | element(size(Call), Call)],
+                    replace_args_for_call(Call, NewArgs);
+                   (Call, ArgsCall) ->
+                    replace_args_for_call(Call, [ArgsCall | element(size(Call), Call)])
+                end, {}, PipedCalls);
+
+parse(Other) -> Other.
+
+% Helpers
 
 replace_args_for_call(Call, Args) ->
     list_to_tuple(lists:droplast(tuple_to_list(Call)) ++ [Args]).
