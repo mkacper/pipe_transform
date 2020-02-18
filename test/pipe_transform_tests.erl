8c60ac34b5799e6665dec23c0fe8fc032f0729f0
Kacper Mentel <mentel.kk@gmail.com>
2020-02-18 16:39:39 +0100

    Add tests

--- test/pipe_transform_tests.erl
+++ test/pipe_transform_tests.erl
@@ -14,5 +14,24 @@ pipe_test_() ->
     [
      ?_assert(pipe_transform:pipe(1,
                                   pipe(),
-                                  pipe(2)) =:= 3)
+                                  pipe()) =:= 1),
+     ?_assert(pipe_transform:pipe(1,
+                                  pipe(1),
+                                  pipe(2)) =:= 4),
+     ?_assert(pipe_transform:pipe(1,
+                                  pipe(9),
+                                  rand:uniform()) =< 10),
+     fun() ->
+        A = 1,
+        ?assert(pipe_transform:pipe(A,
+                                    pipe(),
+                                    pipe()) =:= 1)
+     end,
+     fun() ->
+        A = 1,
+        B = pipe_transform:pipe(A,
+                                pipe(),
+                                pipe()),
+        ?assert(B =:= 1)
+     end
     ].
